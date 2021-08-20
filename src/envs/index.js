3558fd3dcb7c6822fc9e2e3691d7876da4a71c94
FactorMouk <jvoa@cin.ufpe.br>
2021-08-20 12:49:41 -0300

    Adding prod api

--- src/envs/index.js
+++ src/envs/index.js
@@ -1,3 +1,4 @@
 export const env = {
-  api: "http://localhost:8000", //Dev env
+  // api: "http://localhost:8000", //Dev env
+  api: "https://nutcache-challenge-backend.herokuapp.com/", //Prod end
 };
