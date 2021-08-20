a4aa31c2a49f26f6c20cc23985892473b606d640
FactorMouk <jvoa@cin.ufpe.br>
2021-08-19 21:18:14 -0300

    Updating footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,15 @@
 import "./Footer.scss";
 
 export function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <p>A test project by Nutcache</p>
+      <p>
+        Icons by <a href="https://flaticon.com">Flaticon</a>
+      </p>
+      <p>
+        Illustrations by <a href="https://undraw.co/">Undraw</a>
+      </p>
+    </footer>
+  );
 }
